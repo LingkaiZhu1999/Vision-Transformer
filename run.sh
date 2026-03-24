ef641151cad06e2c102db9f42725bb16f1b9b68c
Zhu Lingkai <zhulingkai123@gmail.com>
2026-03-24 08:32:32 +0200

    training shell

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --time=60:00:00
+#SBATCH --mem=80G
+#SBATCH --cpus-per-gpu=20
+#SBATCH --gpus=4
+#SBATCH --ntasks-per-node=1  # <--- Ensures 1 Python command is run per node
+#SBATCH --partition=gpu-h200-141g-ellis
+
+module load scicomp-python-env
+
+# 1. Find the hostname of the first node allocated to the job
+MASTER_NODE=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+
+# 2. Use srun and pass SLURM variables to Python
+srun python main.py \
+  "/scratch/shareddata/dldata/imagenet-1k-wds/imagenet-1k-wds/" \
+  --batch-size 4096 \
+  --workers 16 \
+  --epochs 300 \
+  --weight-decay 0.3 \
+  --lr 3e-3 \
+  --min_lr 1e-6 \
+  --t_warm_up 10000 \
+  --dist-url "tcp://$MASTER_NODE:10001" \
+  --multiprocessing-distributed \
+  --world-size $SLURM_NNODES \
+  --rank $SLURM_NODEID \
+  --dist-backend 'nccl' \
+  --compile \
+  --bf16
